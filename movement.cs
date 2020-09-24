aeaad46a71a106143c3adea9895654a0f8683d2a
hazzad1 <66921724+hazzad1@users.noreply.github.com>
2020-09-24 01:59:10 +0100

    Updated player movement
    flipping in direction and ability to move horizontally

---
+++ movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class movement : MonoBehaviour
+{
+    //necessary for animations and physics
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    //variables to play with
+    public float speed = 2.0f;
+    public float hmovement;
+
+    private bool facingRight = true;
+    
+    
+    // Start is called before the first frame update
+   private void Start()
+    {
+        //define the gameobjects found on the player
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //check input given
+        hmovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    //runs the physics, tied to CPU and not game fps updates
+
+    private void FixedUpdate()
+    {
+        //move character left and right
+        rb2D.velocity = new Vector2(hmovement * speed, rb2D.velocity.y);
+        Flip(hmovement);
+        
+    }
+
+    //flipping function
+    private void Flip(float horizontal)
+    {
+        if (horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+    }
+}
